360ab07e901b9e03818b33d940c679a623139b5a
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-12 19:02:13 -0300

    aula 4

---
+++ Python/aula4.py
@@ -0,0 +1,9 @@
+#bool (boolean) serve para questionar respondendo com true(verdadeiro) ou false(falso)
+#o == serve para verificar se é igual o mesmo produto
+#o = serve para a gente guarda o valor numa variável
+
+print(10 == 10) #sim => True(verdadeiro)
+print(10 == 11) #nao => False(falso)
+print(type(10 == 10))
+print(type(True))
+print(type(False))
