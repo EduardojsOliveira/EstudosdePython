118f9f1b674337e0d4188c3cf41fb8ec734f4464
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-12 15:38:18 -0300

    aula 1 de Python

---
+++ Python/aula1.py
@@ -0,0 +1,7 @@
+#print é ultilizada para exibir as coisas na tela
+#sep é um comando que serve para separa sem o espaço ultilizado o "-" ou um numero
+#/n serve para quebrar a linha
+#end fala o final da linha daquele codigo
+print(11, 22, sep="-", end='\n')
+print(18, 55, sep='-', end='\n')
+print(17, 75, sep='-', end='\n')
