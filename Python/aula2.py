379f3de0d093ee8d098f5e3753d45b68b3557093
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-12 16:04:37 -0300

    aula2

---
+++ Python/aula2.py
@@ -0,0 +1,11 @@
+#Se eu colocar um nome ou escrever algo dentro das aspas simples(') ou duplas(") ele ja identifica com str(string)
+#Para escrever algum texto smepre ultilize aspas simples ou duplas
+#Se colocar \" ou \' ele ignora a aspas e le oque esta escrito
+#Se quiser que a \ aparecera tambem junto com a aspas ou só ela é ultilizado o comando r
+#Normalmente nao vamos ultilizar muito o \' ou o r pois fica um pouco complicado
+#É ultilizado aspas dentro de aspas quando vc querer mostrar os aspas no seu teminal
+print(1234)
+print('Eduardo José')
+print('Eduardo \'José')
+print(r'Eduardo \'José')
+print(2,"Eduardo 'josé'")#O forma que é melhor e mais ultilizada
