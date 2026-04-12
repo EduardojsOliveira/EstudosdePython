0cb721284fa91ea0fe3305bcc6ec3ac23010bb47
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-12 19:31:01 -0300

    aula 5

---
+++ Python/aula5.py
@@ -0,0 +1,10 @@
+#Coerção (Type conversion) seria basicamente converte um tipo para outro tipo
+#str, int, float, bool são tipos imutaveis e primitivos, uma vez criada nao da para alterar no progama
+print(1 + 1)
+print('a' + 'b')
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+print(float('1') + 1)
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
