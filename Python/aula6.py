ad01a25f5f9ceae6ed513ed69c3efbba2a900dae
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-13 19:55:32 -0300

    aula 6

---
+++ Python/aula6.py
@@ -0,0 +1,20 @@
+#Váriaveis serve para salvar algo na memória do seu computador
+#PEP8 indica que a gente inicia as variaveis com letras minúsculas, pode usar números depois das letras minúscalas e underline _.
+# o sinal de = é usado para atribuir um valor a um nome (variável).
+#quando separamos algo com _ ,exemplo, 'nome_variavel' é chamado de snakecase
+#Uso: nome_variavel = expressão
+
+nome_completo = 'Eduardo José'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+int_type_um = type(int_um)
+print(int_type_um)
+print(int_um)
+print(nome_completo, soma_dois_mais_dois)
+print(10 + soma_dois_mais_dois)
+
+nome = 'Eduardo'
+idade = 20
+maior_de_idade = idade >=18
+print('Nome:', nome, 'Idade:', idade)
+print('É de maior:', maior_de_idade)
