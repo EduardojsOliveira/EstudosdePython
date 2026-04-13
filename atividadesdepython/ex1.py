c7d34952e9b4b948521a94f928a78053114be8bb
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-13 20:11:35 -0300

    atividade 1

---
+++ atividadesdepython/ex1.py
@@ -0,0 +1,14 @@
+nome = 'Eduardo'
+sobrenome = 'Oliveira'
+idade = 20
+nascimento = '20/3/2006'
+maior_idade = idade >= 18 
+altura_metros = 1.79
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade", idade)
+print("Data de Nascimento", nascimento)
+print("Maior de Idade:", maior_idade)
+print("Altura em Metros", altura_metros)
+print(f"ola {nome}")
