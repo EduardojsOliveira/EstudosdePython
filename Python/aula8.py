8b6e8f473808084574c2628943cd7a9a43180d1d
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-14 22:34:03 -0300

    aula 8

---
+++ Python/aula8.py
@@ -0,0 +1,12 @@
+concatenacao = 'A' + 'B' + 'C' #quando for utilizar algum desse metodo tomar cuidado com a tipagem str com str, int com int
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 #vai fazer a repetição do str desejado
+tres_vezes_eduardo = 3 * 'Eduardo'
+print(a_dez_vezes)
+print(tres_vezes_eduardo)
+
+
+print('-' * 40)
+print('Hello: ' + 'World')
+print('-' * 40)
