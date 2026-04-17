a6bd99047249efa4e37604b6292ddf376b08929d
Eduardo José <eduardojsoliveira20@gmail.com>
2026-04-17 20:56:23 -0300

    aula 9

--- Python/aula9.py
+++ Python/aula9.py
@@ -4,7 +4,7 @@ print(conta_1)
 #se os operadores tiverem a mesma precendicas sera exucutados da esquerda pra direita.
 #se quisermos outro resultado usaremos o ( ) pois ele vai excutar primerio a operação que estiver dentro dela
 conta_2 = (1 + 1) ** (5 + 5) #aqui podemos ver q a soma foi executada primerio, por que a operação estava dentro do ( )
-print(conta_2)
+print(conta_2) # 1024
 
 conta_3 = (1 + int(0.5 + 0.5)) ** (5 + 5)
-print(conta_3)
+print(conta_3) # 1024
